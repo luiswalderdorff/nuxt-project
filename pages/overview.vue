791d26e74b87d0cbb7c90595157e9547eb8c3e9c
Luis Walderdorff <walderdorff@live.de>
2023-07-17 16:11:24 +0200

    Remove unnecessary link

--- pages/overview.vue
+++ pages/overview.vue
@@ -8,17 +8,12 @@
           <nuxt-link :to="`/details/${product.id}`">
             <img :src="product.thumbnail"
                  class="w-100" />
-            <a href="#!">
-              <div class="mask product-container">
+              <div class="product-container">
                 <div class="d-flex justify-content-between align-items-center h-100 px-2">
                   <h5 class="mb-0"><span class="badge bg-light text-dark mb-2 mt-2">${{ product.price }}</span></h5>
                   <h5 class="mb-0"><span class="badge bg-light text-dark">{{ product.title }}</span></h5>
                 </div>
               </div>
-              <div class="hover-overlay">
-                <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
-              </div>
-            </a>
           </nuxt-link>
         </div>
 
