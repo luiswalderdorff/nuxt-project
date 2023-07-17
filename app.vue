a3bbafc65b22260bc05e5b7324675a53638527a1
Luis Walderdorff <walderdorff@live.de>
2023-07-17 16:08:42 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark px-5">
-      <a class="navbar-brand" href="#">Nuxt</a>
+      <nuxt-link class="navbar-brand" to="/">Nuxt</nuxt-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
               aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -10,10 +10,10 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home</a>
+            <nuxt-link class="nav-link" to="/">Home</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/overview">Overview</a>
+            <nuxt-link class="nav-link" to="/overview">Overview</nuxt-link>
           </li>
         </ul>
       </div>
